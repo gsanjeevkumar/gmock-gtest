44c10d1fe80078bba6635d60d581d59c0e5a3432
Sanjeev Kumar Gadamsetti <sanjeev.gadamsetti@runnertechnologies.com>
2020-12-16 09:47:12 -0500

    updated Compiler Version to 98

--- SearchVector.cpp
+++ SearchVector.cpp
@@ -3,12 +3,20 @@
 
 using namespace std;
 
+void usage()
+{
+    cout<< " Version " << SearchVector_VERSION_MAJOR << "." << SearchVector_VERSION_MINOR <<endl;
+}
+
 int main(int argc, char *argv[])
 {
+
+    usage();
 
     if(argc < 2)
     {
         
+        cout<< "Usage: " << argv[0] << " <INI_File>" <<endl;
+        
     }
-    cout<<"Main"<<endl;
 }
